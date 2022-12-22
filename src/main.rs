24f645b36ecfa82a963286781a51e3f10aba6f81
sty00A4 <99866356+sty00A4@users.noreply.github.com>
2022-12-22 16:12:36 +0100

    test: context vars

--- src/main.rs
+++ src/main.rs
@@ -29,14 +29,32 @@ fn main() {
 #[cfg(test)]
 mod test {
     use super::*;
+
     #[test]
     fn context() {
         let mut context = Context::new();
-        assert_eq!(context.scopes.len(), 1);
-        context.push();
-        assert_eq!(context.scopes.len(), 2);
-        let scope = context.pop();
-        assert!(scope.is_some());
-        assert_eq!(context.scopes.len(), 1);
+        assert_eq!(context.scopes.len(), 1); // first scope exists
+        context.push();                      // second scope
+        assert_eq!(context.scopes.len(), 2); // 2 scopes
+        let scope = context.pop();           // pop second scope
+        assert!(scope.is_some());            // is a scope
+        assert_eq!(context.scopes.len(), 1); // 1 scope
+    }
+    #[test]
+    fn context_vars() {
+        let mut context = Context::new();
+        let path = String::from("<test>");
+        let pos = Position::new(0..0, 0..0, &path);
+        let x = String::from("x");
+        context.create_var(x.clone(), Value::Int(1), false, pos.clone());     // x definition in first scope
+        assert_eq!(context.get_var(&x), Some(&Value::Int(1)));                // x accessable correctly
+        context.push();                                                       // second scope
+        assert_eq!(context.get_var(&x), Some(&Value::Int(1)));                // x still accessable
+        let y = String::from("y"); 
+        context.create_var(y.clone(), Value::Bool(true), false, pos.clone()); // y definition in second scope
+        assert_eq!(context.get_var(&y), Some(&Value::Bool(true)));            // y accessable correctly
+        assert_eq!(context.get_var(&x), Some(&Value::Int(1)));                // x in first scope still accessable
+        context.pop();                                                        // delete second scope
+        assert_eq!(context.get_var(&y), None);                                // y deleted
     }
 }
